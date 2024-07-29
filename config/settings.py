2c5cecb358cf64f6826821484a04798da605a32f
tologonkubandykov <marmokk@icloud.com>
2024-07-29 23:05:02 +0600

    host

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s!%plth48ssgq-b^#%i0m@py867%u(ifjyo3yhu!+ayx$$qjm8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['uuzman.onrender.com']
 
 
 # Application definition
